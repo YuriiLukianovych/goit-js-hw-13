ef61d1010384c28caa8ba8d83748a5badc50be06
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2021-08-05 11:50:08 +0300

    async

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -6,22 +6,26 @@ export default class PixabayApi {
       this.page = 1;
       this.perPage = 40;
    }
-   fetchImages() {
+   async fetchImages() {
       const API_KEY = '1719842-829ac206ae49bf14fe6b0938c';
       const BASE_URL = 'https://pixabay.com/api/';
       const url = `${BASE_URL}?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.perPage}&page=${this.page}`;
 
-      return fetch(url)
-         .then(response => {
-            if (!response.ok) {
-               throw new Error(response.status);
-            }
-            return response.json();
-         })
-         .then(data => {
-            this.page += 1;
-            return data;
-         });
+      const response = await fetch(url);
+      const imagesObj = await response.json();
+      this.page += 1;
+      return imagesObj;
+      // fetch(url)
+      //    .then(response => {
+      //       if (!response.ok) {
+      //          throw new Error(response.status);
+      //       }
+      //       return response.json();
+      //    })
+      //    .then(data => {
+      //       this.page += 1;
+      //       return data;
+      //    });
    }
    resetPage() {
       this.page = 1;
